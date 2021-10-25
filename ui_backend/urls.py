a353352f2e938717d9a0befc2b91fb61c15076be
Marco Buratto <m.buratto@lumit.it>
2021-10-25 10:51:09 +0200

    /history

--- ui_backend/urls.py
+++ ui_backend/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 
-from .controllers import Root, Controller, Authorizations
+from .controllers import Root, Controller, Authorizations, History
 
 
 urlpatterns = [
     path('backend/', Root.RootController.as_view()),
 
     path('backend/authorizations/', Authorizations.AuthorizationsController.as_view()),
+    path('backend/history/', History.HistoryController.as_view()),
 
     re_path(r'^backend/.*/.*', Controller.Controller.as_view()),
 ]
