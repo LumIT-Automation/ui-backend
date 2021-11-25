00d984976ed2e2524965d88eebf93eb791fd867f
mik <m.sartori@lumit.it>
2021-11-25 14:49:57 +0100

    moved here syslog-ng conf and log folder

--- CONTAINER-DEBIAN-PKG/make-release.sh
+++ CONTAINER-DEBIAN-PKG/make-release.sh
@@ -87,24 +87,31 @@ function System_definitions()
         exit 1
     fi
 
-    serviceProjectName="automation-interface-ui-backend_${debPackageRelease}_amd64"
+    shortName="ui-backend"
+    debArch="amd64"
+
+    serviceName="automation-interface-ui-backend"
+    containerName="automation-interface-${shortName}-container"
+    serviceProjectName="${serviceName}_${debPackageRelease}_${debArch}"
     serviceProjectPackage="${workingFolder}/${serviceProjectName}.deb" # inner .deb to be containerized.
 
-    projectName="automation-interface-ui-backend-container_${debPackageRelease}_amd64"
+    projectName="${containerName}_${debPackageRelease}_${debArch}"
     workingFolderPath="${workingFolder}/${projectName}"
 }
 
 
+
 function System_cleanup()
 {
-    if [ -n "$workingFolderPath" ]; then
-        if [ -d "$workingFolderPath" ]; then
-            rm -fR "$workingFolderPath"
+    # List of the directories to be deleted.
+    rmDirs="$workingFolderPath ${workingFolder}/${containerName} ${workingFolder}/${serviceProjectName} ${workingFolder}/${projectName}"
+    for dir in $rmDirs; do
+        if [ -d "$dir" ]; then
+            rm -fR "$dir"
         fi
-
-        mkdir $workingFolderPath
-    fi
+    done
 }
+
 
 
 function System_serviceDebCreate()
@@ -116,14 +123,24 @@ function System_serviceDebCreate()
 function System_systemFilesSetup()
 {
     # Setting up system files.
+    mkdir "$workingFolderPath"
+
+    # Setting up system files.
     cp -R usr $workingFolderPath
     cp -R etc $workingFolderPath
-    mv $serviceProjectPackage $workingFolderPath/usr/lib/ui-backend/
+    cp -R var $workingFolderPath
 
-    sed -i "s/PACKAGE/${serviceProjectName}.deb/g" $workingFolderPath/usr/lib/ui-backend/Dockerfile
+    # Cleanup.
+    rm -f $workingFolderPath/var/log/automation/uib/placeholder
 
-    chmod +x $workingFolderPath/usr/bin/ui-backend-container.sh
-    chmod +x $workingFolderPath/usr/lib/ui-backend/bootstrap.sh
+    mv $serviceProjectPackage $workingFolderPath/usr/lib/${shortName}
+    sed -i "s/PACKAGE/${serviceProjectName}.deb/g" $workingFolderPath/usr/lib/${shortName}/Dockerfile
+
+    find "$workingFolderPath" -type d -exec chmod 0755 {} \;
+    find "$workingFolderPath" -type f -exec chmod 0644 {} \;
+
+    chmod +x $workingFolderPath/usr/bin/${shortName}-container.sh
+    chmod +x $workingFolderPath/usr/lib/${shortName}/bootstrap.sh
 }
 
 
