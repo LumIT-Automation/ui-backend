36f0f1e86bb0322a60ca0f7afac0b23c743fc017
mik <m.sartori@lumit.it>
2022-10-06 11:53:31 +0200

    CheckPointRemoveHost serializer

--- ui_backend/serializers/usecases/CheckPointRemoveHost.py
+++ ui_backend/serializers/usecases/CheckPointRemoveHost.py
@@ -13,8 +13,24 @@ class CheckPointRemoveHostSerializer(serializers.Serializer):
             def __init__(self, *args, **kwargs):
                 super().__init__(*args, **kwargs)
 
-                for technology in settings.API_BACKEND_BASE_URL:
-                    self.fields[technology] = serializers.ListField(child=serializers.IntegerField(required=False), required=False)
+            def to_internal_value(self, data):
+                try:
+                    # These fields can be a string or a list of string.
+                    for technology in settings.API_BACKEND_BASE_URL:
+                        if technology in data:
+                            if isinstance(data[technology], str):
+                                if data[technology] != "*":
+                                    raise ValueError('   Invalid data. The  only allowed string value in this field is "*".' )
+                                else:
+                                    self.fields[technology] = serializers.CharField(max_length=1, required=False)
+                            elif isinstance(data[technology], list):
+                                self.fields[technology] = serializers.ListField(child=serializers.IntegerField(required=False), required=False)
+                            else:
+                                raise ValueError('   Invalid data. Expected a string or a list, but got ' + str(type(data[technology])))
+
+                    return super().to_internal_value(data)
+                except Exception as e:
+                    raise e
 
         self.fields["ipv4-address"] = serializers.IPAddressField(protocol='IPv4', required=True)
         self.fields["asset"] = CheckPointRemoveHostAssetSerializer(required=True)
