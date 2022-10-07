0f3115be934dcaf53357cd1d226ace24e208ee63
mik <m.sartori@lumit.it>
2022-10-07 11:00:46 +0200

    CheckPointRemoveHost: assetId from PUT data

--- ui_backend/usecases/CheckPointRemoveHost.py
+++ ui_backend/usecases/CheckPointRemoveHost.py
@@ -1,5 +1,7 @@
 from ui_backend.usecases.Workflow import Workflow
 
+from ui_backend.helpers.Exception import CustomException
+
 
 class CheckPointRemoveHost(Workflow):
     def __init__(self, data: dict, username: str, workflowId: str, *args, **kwargs):
@@ -17,27 +19,19 @@ class CheckPointRemoveHost(Workflow):
 
     def __call__(self):
         try:
-            # API requests with "workflow" system user.
-            #technology = "vmware"
-            #urlSegment = "1/datacenters/"
+            assets = self.data["asset"]
+            for assetId in assets["checkpoint"]:
+                technology = "checkpoint"
+                urlSegment = assetId + "/remove-host/"
 
-            #data = self.requestFacade(
-            #    method="GET",
-            #    technology=technology,
-            #    urlSegment=urlSegment
-            #)
-
-            # Todo: pass assetIds from the asset field in the request.
-            assetId = "1"
-            technology = "checkpoint"
-            urlSegment = assetId + "/remove-host/"
-
-            data = self.requestFacade(
-                method="PUT",
-                technology=technology,
-                urlSegment=urlSegment,
-                data=self.data
-            )
+                data = self.requestFacade(
+                    method="PUT",
+                    technology=technology,
+                    urlSegment=urlSegment,
+                    data=self.data
+                )
+        except KeyError:
+            raise CustomException(status=400, payload={"UI-BACKEND": "At least one checkpoint asset is required to complete this workflow."})
         except Exception as e:
             raise e
 
