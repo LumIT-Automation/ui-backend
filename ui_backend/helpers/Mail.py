07ea132edc10ce3dd5984ee8ad5470b3d4d07dcd
mik <m.sartori@lumit.it>
2026-02-06 10:39:44 +0100

    mail helper

---
+++ ui_backend/helpers/Mail.py
@@ -0,0 +1,20 @@
+from django.core.mail import send_mail
+
+from checkpoint.helpers.Log import Log
+
+
+class Mail:
+    @staticmethod
+    def send(user: dict, subject: str, message: str) -> None:
+        try:
+            Log.actionLog("Sending email", user)
+
+            send_mail(
+                subject,
+                message,
+                'default-from@automation.local',
+                ['default-to@automation.local'],
+                fail_silently=False,
+            )
+        except Exception as e:
+            Log.log("Sending email failed: "+str(e.__str__()))
